c49e8f7b5de7d6438d84a5fdcb4f1e10a7be17d5
vinaykomaravolu <vinay.komaravolu@mail.utoronto.ca>
2019-02-23 02:25:33 -0500

    init

---
+++ Include/filewatcher.h
@@ -0,0 +1,64 @@
+#pragma once
+#include <iostream>
+#include <chrono>
+#include <filesystem>
+#include <experimental/filesystem>
+#include <thread>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+namespace fs = experimental::filesystem;
+using namespace std::chrono_literals;
+class FileWatcher {
+public:
+	string pathWatch;
+	unordered_map<string, time_t> fileWriteTimes;
+
+	FileWatcher(string pathWatch) {
+		this->pathWatch = pathWatch;
+		for (auto &p : fs::recursive_directory_iterator(pathWatch)) {
+
+			//Check and convert current paths' last write time 
+			auto ftime = fs::last_write_time(p.path());
+			time_t cftime = decltype(ftime)::clock::to_time_t(ftime);
+			//auto lastWriteTime =  std::asctime(std::localtime(&cftime));
+
+			//add path to unordered map
+			fileWriteTimes.insert(make_pair(p.path().string(), cftime));
+		}
+	}
+
+	void watch() {
+		unordered_map<string, time_t> tempfileWriteTimes;
+		for (auto &p : fs::recursive_directory_iterator(pathWatch)) {
+			fs::path currentPath = p.path();
+			auto ftime = fs::last_write_time(currentPath);
+			time_t currentLastWriteTime = decltype(ftime)::clock::to_time_t(ftime);
+			//check if path exists in map
+			//if path does not exist then it is created or modified
+			if (fileWriteTimes.count(currentPath.string()) == 0) {
+				tempfileWriteTimes.insert(make_pair(currentPath.string(), currentLastWriteTime));
+				cout << currentPath << endl;
+			}
+			time_t previousLastWriteTime = fileWriteTimes[currentPath.string()];
+
+			//if the file has been changed then print the path and return from the function
+			//also update path last write time
+			if (previousLastWriteTime != currentLastWriteTime) {
+				tempfileWriteTimes.insert(make_pair(currentPath.string(), currentLastWriteTime));
+				cout << currentPath << endl;
+			}
+			else {
+				tempfileWriteTimes.insert(make_pair(currentPath.string(), previousLastWriteTime));
+			}
+		}
+		fileWriteTimes = tempfileWriteTimes;
+	}
+
+	void displayAllPaths() {
+		for (auto iter = fileWriteTimes.begin(); iter != fileWriteTimes.end(); ++iter) {
+			cout << iter->first << endl;
+		}
+	}
+};
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <filewatcher.h>
+#include <thread>
+
+using namespace std;
+
+int main() {
+	FileWatcher fw("C:/Users/Main/Desktop/CS 3rd year/CSC309");
+	while (true) {
+		this_thread::sleep_for(2s);
+		fw.watch();
+	}
+	
+	return 0;
+}
