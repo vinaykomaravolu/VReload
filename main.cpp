e7cdfaae9dcf28833b98be8ec1f4a7a799deaf9c
vinaykomaravolu <vinay.komaravolu@mail.utoronto.ca>
2019-03-07 01:08:38 -0500

    finished prototype of reload functionality

--- main.cpp
+++ main.cpp
@@ -8,7 +8,17 @@
 #define MAKEFILE "CC		:= g++\nC_FLAGS := -std=c++17 -Wall -Wextra\n\nBIN		:= bin\nSRC		:= src\nINCLUDE	:= include\nLIB		:= lib\n\nLIBRARIES	:=\n\nifeq ($(OS),Windows_NT)\nEXECUTABLE	:= main.exe\nelse\nEXECUTABLE	:= main\nendif\n\nall: $(BIN)/$(EXECUTABLE)\n\nclean:\n	$(RM) $(BIN)/$(EXECUTABLE)\n\nrun: all\n	./$(BIN)/$(EXECUTABLE)\n\n$(BIN)/$(EXECUTABLE): $(SRC)/*\n	$(CC) $(C_FLAGS) -I$(INCLUDE) -L$(LIB) $^ -o $@ $(LIBRARIES)"
 #define SIMPLEMAINFILE "#include <iostream>\nusing namespace std;\n\nint main(){\n\n\n}"
 
+void clearScreen()
+{
+	std::system("cls");
+}
+
+
 // Process Currently Running
+bool batchProcessRunning = false;
+bool executableProcessRunning = false;
+HANDLE hBatchProcess = NULL;
+HANDLE hExecutableProcess = NULL;
 
 void createDirectoryHelper(fs::path path) {
 	if (!fs::exists(path) || !fs::is_directory(path)) {
@@ -26,63 +36,76 @@ void createFileHelper(fs::path path,string textToAdd) {
 }
 
 int makeProject() {
-	STARTUPINFO siBat;
-	PROCESS_INFORMATION piBat;
+	if (!batchProcessRunning) {
+		clearScreen(); // RISKY/UNSTABLE
 
-	ZeroMemory(&siBat, sizeof(siBat));
-	siBat.cb = sizeof(siBat);
-	ZeroMemory(&piBat, sizeof(piBat));
-	if (!CreateProcess(NULL,
-		(LPSTR)"run.bat",
-		NULL,
-		NULL,
-		FALSE,
-		0,
-		NULL,
-		NULL,
-		&siBat,
-		&piBat)
-		)
-	{
-		printf("CreateProcess failed (%d)\n", GetLastError());
-		return FALSE;
-	}
-	cout << piBat.dwProcessId << endl;
-	WaitForSingleObject(piBat.hProcess,INFINITE);
-	CloseHandle(piBat.hProcess);
-	CloseHandle(piBat.hThread);
+		// If executable process is still running then close the current one
+		if (hExecutableProcess) {
+			TerminateProcess(hExecutableProcess, 0);
+		}
 
-	STARTUPINFO siExec;
-	PROCESS_INFORMATION piExec;
+		// Run the batch file
+		batchProcessRunning = true;
+		STARTUPINFO siBat;
+		PROCESS_INFORMATION piBat;
 
-	ZeroMemory(&siExec, sizeof(siExec));
-	siExec.cb = sizeof(siExec);
-	ZeroMemory(&piExec, sizeof(piExec));
-
-	// Checking to see if the exe is created or failed
-	fs::path currentDirectory = fs::current_path();
-	fs::path exeFile(currentDirectory.string() + "\\bin\\main.exe");
-	if (fs::exists(exeFile)) {
+		ZeroMemory(&siBat, sizeof(siBat));
+		siBat.cb = sizeof(siBat);
+		ZeroMemory(&piBat, sizeof(piBat));
 		if (!CreateProcess(NULL,
-			(LPSTR)"bin\\main.exe",
+			(LPSTR)"run.bat",
 			NULL,
 			NULL,
 			FALSE,
-			0,
+			CREATE_NO_WINDOW,
 			NULL,
 			NULL,
-			&siExec,
-			&piExec)
+			&siBat,
+			&piBat)
 			)
 		{
 			printf("CreateProcess failed (%d)\n", GetLastError());
 			return FALSE;
 		}
-		cout << piExec.dwProcessId << endl;
-		WaitForSingleObject(piExec.hProcess, INFINITE);
-		CloseHandle(piExec.hProcess);
-		CloseHandle(piExec.hThread);
-		
+		WaitForSingleObject(piBat.hProcess, INFINITE);
+		CloseHandle(piBat.hProcess);
+		CloseHandle(piBat.hThread);
+		batchProcessRunning = false;
+
+
+		// Executable Process
+		STARTUPINFO siExec;
+		PROCESS_INFORMATION piExec;
+
+		ZeroMemory(&siExec, sizeof(siExec));
+		siExec.cb = sizeof(siExec);
+		ZeroMemory(&piExec, sizeof(piExec));
+
+		// Checking to see if the exe is created or failed
+		fs::path currentDirectory = fs::current_path();
+		fs::path exeFile(currentDirectory.string() + "\\bin\\main.exe");
+		if (fs::exists(exeFile)) {
+			if (!CreateProcess(NULL,
+				(LPSTR)"bin\\main.exe",
+				NULL,
+				NULL,
+				FALSE,
+				0,
+				NULL,
+				NULL,
+				&siExec,
+				&piExec)
+				)
+			{
+				printf("CreateProcess failed (%d)\n", GetLastError());
+				return FALSE;
+			}
+			hExecutableProcess = piExec.hProcess;
+			WaitForSingleObject(piExec.hProcess, INFINITE);
+			hExecutableProcess = NULL;
+			CloseHandle(piExec.hProcess);
+			CloseHandle(piExec.hThread);
+		}
 	}
 	return 0;
 }
@@ -105,7 +128,7 @@ int main() {
 	createDirectoryHelper(srcDirectory);
 	createFileHelper(makeFile, MAKEFILE);
 	createFileHelper(mainFile, SIMPLEMAINFILE);
-	createFileHelper(batFILE, "mingw32-make\nIF \"%ERRORLEVEL%\" == \"0\" (\n\tbin\\main.exe\n)");
+	createFileHelper(batFILE,"mingw32-make\nIf %ERRORLEVEL% NEQ 0 (\n\tdel \"bin\\main.exe\"\n)");
 
 	FDWatcher::Watcher w;
 	w.watchFile(currentDirectory.string() + "\\src", makeProject);
